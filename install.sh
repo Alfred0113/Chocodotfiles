195b1567758ce0e9f2455c9bd9048c17cd8cfddb
Alfred0113 <Alfred0113@users.noreply.github.com>
2026-09-06 18:20:10 -0600

    install.sh: agrega sddm/hypridle/hyprlock/uwsm/polkit-gnome/fish/tmux a PKGS_REPO
    En una instalación limpia el script habilita sddm.service, el autostart
    lanza hypridle y polkit-gnome, y chocomazapan-system-lock llama a hyprlock,
    pero ninguno estaba en la lista de paquetes. Se agregan junto con fish (la
    shell) y tmux.

--- install.sh
+++ install.sh
@@ -95,7 +95,7 @@ done
 # en el repo cachyos). PKGS_AUR necesitan un helper (paru/yay). Los
 # opcionales no se instalan solos. Se corre antes de generar el tema
 # porque esa parte necesita 'aether'.
-PKGS_REPO="hyprland waybar mako walker quickshell awww alacritty swayosd brightnessctl playerctl openrgb chafa imagemagick fastfetch socat grim jq ttf-jetbrains-mono-nerd btop hunspell nautilus-python"
+PKGS_REPO="hyprland hypridle hyprlock uwsm sddm polkit-gnome fish tmux waybar mako walker quickshell awww alacritty swayosd brightnessctl playerctl openrgb chafa imagemagick fastfetch socat grim jq ttf-jetbrains-mono-nerd btop hunspell nautilus-python"
 PKGS_AUR="aether python-terminaltexteffects vencord-installer-git elephant elephant-bluetooth elephant-calc elephant-clipboard elephant-desktopapplications elephant-files elephant-menus elephant-providerlist elephant-runner elephant-symbols elephant-todo elephant-unicode elephant-websearch"
 PKGS_OPT="kitty foot ghostty mise"
 
